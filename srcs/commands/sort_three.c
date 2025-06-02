8a8dd16a72061e43789904d037497bc23d897d6f
Aftel4 <noegerace@gmail.com>
2025-06-02 15:57:39 +0200

    1

---
+++ srcs/commands/sort_three.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_three.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 19:58:53 by marvin            #+#    #+#             */
+/*   Updated: 2025/05/27 19:58:53 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_three(t_stack_node **a)
+{
+	t_stack_node	*biggest_node;
+
+	biggest_node = find_max(*a);
+	if (biggest_node == *a)
+		ra(a, false);
+	else if ((*a)->next == biggest_node)
+		rra(a, false);
+	if ((*a)->nbr > (*a)->next->nbr)
+		sa(a, false);
+}
